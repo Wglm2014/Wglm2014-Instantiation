93d59618f385483ee019dd159a7cb229801c1dc7
Wilson Linares <Glinaresmiranda@yahoo.com>
2023-02-20 21:53:41 +0000

    new object StringBuffer created

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+         StringBuffer sB = new StringBuffer("my name is Wilson");
+        return sB;
     }
 }
